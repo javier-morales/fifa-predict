bce3b1828a281545dcc3b50f8bf650eced098dc3
Ibrar Malik <9162284+ibrarmalik@users.noreply.github.com>
2019-04-11 16:06:00 +0200

    Add data preprocessing

--- projecte_beta.R
+++ projecte_beta.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(tidyr)
+library(readr)
 
 data <- read.csv("fifa.csv")
 
@@ -7,6 +9,18 @@ data <- read.csv("fifa.csv")
 # --- Preprocessing ---
 
 data <- data %>%
-  select(-one_of("X", "ID", "Photo", "Flag", "Club.Logo", "Jersey.Number",
-                 "Joined", "Loaned.From", "Contract.Valid.Until")) %>%
-  filter(complete.cases(data))
+  select(-one_of("X", "ID", "Photo", "Flag", "Club.Logo",
+                 "Real.Face", "Jersey.Number", "Joined",
+                 "Loaned.From", "Contract.Valid.Until","Body.Type")) %>%
+  filter(complete.cases(data)) %>%
+  filter(Position != "GK") %>%
+  separate(Height, c('ft', 'inch'), "'", convert = TRUE) %>%
+  separate(Work.Rate, c('WR.Attack', 'WR.Defense'), "/ ") %>%
+  mutate(Value = parse_number(Value),
+         Wage = parse_number(Value),
+         Release.Clause = parse_number(Value),
+         Weight = parse_number(Value) * 0.453592,  # lbs to kg
+         Height = ft*30.48 + inch*2.54
+  ) %>%
+  mutate_at(vars(LS:RB), funs(parse_number))
+
