a1ded8a215b6f7d132a721e3507333053497ddde
Ibrar Malik <9162284+ibrarmalik@users.noreply.github.com>
2019-04-11 15:11:45 +0200

    Select variables and delete NAs

--- projecte_beta.R
+++ projecte_beta.R
@@ -1,1 +1,12 @@
+library(dplyr)
+
 data <- read.csv("fifa.csv")
+
+
+
+# --- Preprocessing ---
+
+data <- data %>%
+  select(-one_of("X", "ID", "Photo", "Flag", "Club.Logo", "Jersey.Number",
+                 "Joined", "Loaned.From", "Contract.Valid.Until")) %>%
+  filter(complete.cases(data))
