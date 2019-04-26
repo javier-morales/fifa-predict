8656e1a2d4c9cbab78ea8841764a3ec4302158c0
Ibrar Malik <9162284+ibrarmalik@users.noreply.github.com>
2019-04-26 22:22:56 +0200

    Simplify and group variables

--- projecte_beta.R
+++ projecte_beta.R
@@ -6,9 +6,12 @@ library(cclust)
 data <- read.csv("fifa.csv")
 
 
+# ---------------------
+# --- PREPROCESSING ---
+# ---------------------
 
-# --- Preprocessing ---
-
+# We first remove useless variables.
+# Goalkeepers (NAs) and their stats are also removed
 
 data <- data %>%
   select(-one_of("X", "ID", "Photo", "Flag", "Club.Logo",
@@ -16,6 +19,11 @@ data <- data %>%
                  "Loaned.From", "Contract.Valid.Until","Body.Type")) %>%
   filter(complete.cases(data)) %>%
   filter(Position != "GK") %>%
+  select(-one_of("GKDiving", "GKHandling", "GKKicking", "GKPositioning", "GKReflexes")) 
+
+# Next we change units, parse numbers and separate variables that merged
+
+data <- data %>%
   separate(Height, c('ft', 'inch'), "'", convert = TRUE) %>%
   separate(Work.Rate, c('WR.Attack', 'WR.Defense'), "/ ") %>%
   mutate(Value = parse_number(Value),
@@ -23,12 +31,16 @@ data <- data %>%
          Weight = parse_number(Weight) * 0.453592,  # lbs to kg
          Height = ft*30.48 + inch*2.54
   ) %>%
-  mutate_at(vars(LS:RB), funs(parse_number)) %>%
-  mutate_at(vars(Weak.Foot, Skill.Moves, WR.Attack, WR.Defense,), funs(as.factor)) %>%
-  select(-one_of("ft", "inch"))
+  select(-one_of("ft", "inch")) %>%
+  mutate_at(vars(LS:RB), parse_number) %>%
+  mutate_at(vars(Weak.Foot, Skill.Moves, WR.Attack, WR.Defense), factor, ordered = TRUE)
 
-#data <- data[0:500,]
+levels(data$WR.Attack) <- c("Low", "Medium", "High")  # Correct order
+levels(data$WR.Defense) <- c("Low", "Medium", "High")
 
+# Now we will group some factors and variables together.
+# We are going to create the 'Role' variable using 'Position'. It will
+# only have three levels: ATT, MID and DEF.
 
 data$Role <- data$Position
 levels(data$Role) <- c("", "ATT", "DEF", "MID", "ATT", "MID",
@@ -38,8 +50,40 @@ levels(data$Role) <- c("", "ATT", "DEF", "MID", "ATT", "MID",
             "ATT", "ATT", "DEF", "ATT")
 data$Position <- NULL
 
+# We are going to group  all the different habilitiy variables (crossing, dribbling) into
+# more simple groups, according to the dataset source (sofifa.com): Attacking, Skill, 
+# Movement, Power, Mentality and Defending.
 
-# --- resampling data ---
+data$Attacking <- rowMeans(data[,c("Crossing", "Finishing", "HeadingAccuracy",
+                                   "ShortPassing", "Volleys")])
+data$Skill <- rowMeans(data[, c("Dribbling", "Curve", "FKAccuracy",
+                                "LongPassing", "BallControl")])
+data$Movement <- rowMeans(data[,c("Acceleration", "SprintSpeed", "Agility",
+                                  "Reactions", "Balance")])
+data$Power <- rowMeans(data[, c("ShotPower", "Jumping", "Stamina", "Strength",
+                                "LongShots")])
+data$Mentality <- rowMeans(data[, c("Aggression", "Interceptions","Positioning",
+                                    "Vision", "Penalties", "Composure")])
+data$Defending <- rowMeans(data[, c("Marking", "StandingTackle", "SlidingTackle")])
+
+data <- select(data, -(Crossing:SlidingTackle)) # We remove them!
+
+# We do the same thing for the position score of the players, the same
+# way we did when making the 'Role' variable.
+
+data$ATT <- rowMeans(data[, c("CAM", "CF", "LAM", "LF","LS",
+                              "LW", "RF", "RS", "RW", "ST")])
+data$MID <- rowMeans(data[, c("CDM", "CM", "LCM", "LDM","LM",
+                              "RAM", "RCM", "RDM", "RM")])
+data$DEF <- rowMeans(data[, c("CB", "LB", "LCB","LWB",
+                              "RB", "RCB", "RWB")])
+
+data <- select(data, -(LS:RB)) # We remove them!
+
+
+# -----------------------
+# --- RESAMPLING DATA ---
+# -----------------------
 
 # 1. shuffle data
 data <- data[sample(nrow(data)),]
@@ -51,14 +95,18 @@ test <- data[1:bound,]
 train <- data[(bound+1):nrow(data),]
 
 
-# --- data matrix ---
+# -------------------
+# --- DATA MATRIX ---
+# -------------------
 
 numerical <- !sapply(data, is.factor)
 
 data.m <- sapply(data[,numerical], as.numeric)
 data.m <- scale(data.m)
 
+# --------------
 # ---- PCA -----
+# --------------
 
 comp <- princomp(data.m)
 screeplot(comp)
@@ -71,15 +119,10 @@ par(mfrow = c(1, 2))
 plot(comp$scores[,1], comp$scores[,2], col = kmeans.3$cluster, 
      main = "K-means", xlab = "Comp1", ylab = "Comp2")
 points(comp$scores[1:10,1], comp$scores[1:10,2], pch = 8, col = "gold")
-plot(comp$scores[,1], comp$scores[,2], col = data$Role , main = "Role",
+plot(comp$scores[,1], comp$scores[,2], col = cut(data$Overall, breaks = 3) , main = "Role",
      xlab = "Comp1", ylab = "Comp2")
 points(comp$scores[1:10,1], comp$scores[1:10,2], pch = 8, col = "gold")
 par(mfrow = c(1, 1))
-
-#COMENTAR LO DEL MIG DEL CAMP QUE SON UNA MESCLA DE ATAC I DEFENSA
-#I QUE EL NOMBRE DE CLUSTERS ES TRES I TAL
-#HEM "PREDIT" EL ROL DEL JUGADOR
-
 
 points(comp$scores[1:10,1], comp$scores[1:10,2], pch = 8)
 
