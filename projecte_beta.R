c6734f8e4b9b71f9c7f14fa4303fb4fca8e63aa0
Javier Morales <38464108+javier-morales@users.noreply.github.com>
2019-04-30 15:15:12 +0200

    Add linear models

--- projecte_beta.R
+++ projecte_beta.R
@@ -169,7 +169,11 @@ par(mfrow = c(1, 1))
 #----DATA VISUALIZATION II--#
 #-----------------
 
+library(graphics)
 Star.var <- data.m[,11:16]
+Star.Best <- data.m[1:5,11:16]
+
+stars(Star.Best, scale = T)
 
 # --------------
 # -- LASSO -----
@@ -193,6 +197,45 @@ p.te <- predict(mod.lasso, newx = test.m[,-4], s = "lambda.min")
 # R squared gives us an NRMSE of 0.1541901 with the test data
 (NRMSE.test <- sqrt(1 - sum((p.te - mean(test.m[, 4]))^2) / sum((test.m[, 4] - mean(test.m[, 4]))^2)))
 
+
+#-----------------
+#-------LM--------
+#-----------------
+try.lm <- lm(Value~Age+Overall+Potential+Wage+International.Reputation+Skill.Moves+Pace+Shooting+Passing
+             + Dribbles + Defending+ Physicality, data= train)
+
+summary(try.lm)
+st <- step(try.lm)
+
+#The model using Step algorithm is:
+st.lm <- lm(Value ~ Age + Overall + Potential + Wage + International.Reputation + 
+  Skill.Moves + Shooting + Passing + Defending, data = train)
+
+summary(st.lm)
+
+p.tr <- predict(st.lm, newx = x)
+p.te <- predict(st.lm,  newx = test.m[,-4])
+
+# R squared gives us an NRMSE of 0.2007968 with the test data
+(NRMSE.train <- sqrt(1 - sum((p.tr - mean(train.m[, 4]))^2) / sum((train.m[, 4] - mean(train.m[, 4]))^2)))
+
+# R squared gives us an NRMSE of 0.1541901 with the test data
+(NRMSE.test <- sqrt(1 - sum((p.te - mean(test.m[, 4]))^2) / sum((test.m[, 4] - mean(test.m[, 4]))^2)))
+
+#Now let's try a new linear model with LASSO variables with non-zero coefficients.
+
+l.lm <- lm(Value~ Overall + Potential + Wage+ International.Reputation + Skill.Moves + Pace, data = train)
+summary(l.lm)
+#79,29 % with less than the half of the variables.
+
+p.tr <- predict(l.lm, newx = x)
+p.te <- predict(l.lm,  newx = test.m[,-4])
+
+# R squared gives us an NRMSE of 0.2007968 with the test data
+(NRMSE.train <- (1 - sum((p.tr - mean(train.m[, 4]))^2) / sum((train.m[, 4] - mean(train.m[, 4]))^2)))
+
+# R squared gives us an NRMSE of 0.1541901 with the test data
+(NRMSE.test <- (1 - sum((p.te - mean(test.m[, 4]))^2) / sum((test.m[, 4] - mean(test.m[, 4]))^2)))
 
 # --
 # Lasso predict potential
